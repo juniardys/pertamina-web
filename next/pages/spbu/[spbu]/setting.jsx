cdfc7ac3786c39ea225b125ef2b317a25a70cbe4
Fariz <nizaralfariziakbar10@gmail.com>
2020-02-21 11:37:40 +0700

    setting page

--- next/pages/spbu/[spbu]/setting.jsx
+++ next/pages/spbu/[spbu]/setting.jsx
@@ -7,6 +7,46 @@ class Setting extends Component {
         return { query }
     }
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            uuid: 'qwer1234',
+            name: 'Pertamina G-WALK',
+            code: 'PRTMNGWLK',
+            phone: '085102725497',
+            address: 'G-Walk',
+            user: 0,
+            isLoading: true,
+        }
+    }
+
+    handleInputChange = async (e) => {
+        await this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    _deleteSPBU = async (uuid) => {
+        Swal.fire({
+            title: 'Apakah anda yakin?',
+            text: "Anda tidak akan dapat mengembalikan ini!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya, hapus!',
+            cancelButtonText: 'Batal'
+        }).then((result) => {
+            if (result.value) {
+                Swal.fire('Berhasil!', 'SPBU berhasil dihapus.', 'success')
+            }
+        })
+    }
+
+    _submit = async (uuid) => {
+        console.log(uuid);
+    }
+
     render() {
         const breadcrumb = [
             {
@@ -18,11 +58,99 @@ class Setting extends Component {
                 url: `/spbu/${this.props.query.spbu}/report`
             }
         ]
-        
-    
+
         return (
             <Layout title={'Pengaturan  ' + this.props.query.spbu} breadcrumb={breadcrumb}>
-                ini pengaturan spbu {this.props.query.spbu}
+                <form action="#">
+                    <div className="panel panel-flat">
+                        <div className="panel-heading">
+                            <h5 className="panel-title">Detail SPBU <a className="heading-elements-toggle"><i className="icon-more"></i></a></h5>
+                            <div className="heading-elements">
+                                <ul className="icons-list">
+                                    <li><a data-action="collapse"></a></li>
+                                    <li><a data-action="reload"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="panel-body">
+                            <div className="form-group row">
+                                <label className="control-label col-lg-2">Nama</label>
+                                <div className="col-lg-10">
+                                    <input type="text" className="form-control" name="name" value={this.state.name} onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <label className="control-label col-lg-2">Kode</label>
+                                <div className="col-lg-10">
+                                    <input type="text" className="form-control" name="code" value={this.state.code} onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <label className="control-label col-lg-2">Alamat</label>
+                                <div className="col-lg-10">
+                                    <input type="text" className="form-control" name="address" value={this.state.address} onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+                            <div className="form-group row">
+                                <label className="control-label col-lg-2">Nomor Handphone</label>
+                                <div className="col-lg-10">
+                                    <input type="text" className="form-control" name="phone" value={this.state.phone} onChange={this.handleInputChange} />
+                                </div>
+                            </div>
+
+                            <div className="text-right">
+                                <button type="submit" className="btn btn-primary">Ubah</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+                <form action="#">
+                    <div className="panel panel-flat">
+                        <div className="panel-heading">
+                            <h5 className="panel-title">Metode Pembayaran <a className="heading-elements-toggle"><i className="icon-more"></i></a></h5>
+                            <div className="heading-elements">
+                                <ul className="icons-list">
+                                    <li><a data-action="collapse"></a></li>
+                                    <li><a data-action="reload"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div className="panel-body">
+                            <div className="form-group row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="text-semibold">Pilih Metode Pembayaran yang ditetapkan pada SPBU ini</label>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" value="tunai" defaultChecked="checked" />
+                                                Tunai
+											</label>
+                                        </div>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" value="cc" />
+                                                CC
+											</label>
+                                        </div>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" value="voucher" />
+                                                Voucher
+											</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="text-right">
+                                <button type="submit" className="btn btn-primary">Tetapkan</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </Layout>
         )
     }
