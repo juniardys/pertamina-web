768c20d2fe651f2d78f5f2d9ce927655c1133d92
Fariz <nizaralfariziakbar10@gmail.com>
2020-02-26 11:30:23 +0700

    revision 2020-02-26

--- next/pages/index.jsx
+++ next/pages/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Layout from "~/components/layouts/Base";
+import Link from "next/link"
 
 class Index extends Component {
     render() {
@@ -7,63 +8,64 @@ class Index extends Component {
             <Layout title="Dashboard">
                 <div className="row">
                     <div className="col-lg-4">
+                        <Link href="/user">
+                            <div className="panel bg-teal-400 card-panel-dashboard">
+                                <div className="panel-body">
+                                    <div className="heading-elements">
+                                        {/* <span className="heading-text badge bg-teal-800">+53,6%</span> */}
+                                    </div>
 
-                        <div className="panel bg-teal-400">
-                            <div className="panel-body">
-                                <div className="heading-elements">
-                                    {/* <span className="heading-text badge bg-teal-800">+53,6%</span> */}
-                                </div>
-
-                                <h3 className="no-margin">3,450</h3>
-                                User
+                                    <h3 className="no-margin">1</h3>
+                                    User
                                 <div className="text-muted text-size-small">Total Jumlah User</div>
-                            </div>
+                                </div>
 
-                            <div className="container-fluid">
-                                <div id="members-online"></div>
+                                <div className="container-fluid">
+                                    <div id="members-online"></div>
+                                </div>
                             </div>
-                        </div>
-
+                        </Link>
                     </div>
 
                     <div className="col-lg-4">
+                        <Link href="/spbu">
+                            <div className="panel bg-pink-400 card-panel-dashboard">
+                                <div className="panel-body">
+                                    <div className="heading-elements">
+                                        {/* <span className="heading-text badge bg-teal-800">+53,6%</span> */}
+                                    </div>
 
-                        <div className="panel bg-pink-400">
-                            <div className="panel-body">
-                                <div className="heading-elements">
-                                    {/* <span className="heading-text badge bg-teal-800">+53,6%</span> */}
-                                </div>
-
-                                <h3 className="no-margin">1</h3>
-                                SPBU
+                                    <h3 className="no-margin">1</h3>
+                                    SPBU
                                 <div className="text-muted text-size-small">Total Jumlah SPBU</div>
-                            </div>
+                                </div>
 
-                            <div className="container-fluid">
-                                <div id="members-online"></div>
+                                <div className="container-fluid">
+                                    <div id="members-online"></div>
+                                </div>
                             </div>
-                        </div>
+                        </Link>
 
                     </div>
 
                     <div className="col-lg-4">
+                        <Link href="/product">
+                            <div className="panel bg-blue-400 card-panel-dashboard">
+                                <div className="panel-body">
+                                    <div className="heading-elements">
+                                        {/* <span className="heading-text badge bg-teal-800">+53,6%</span> */}
+                                    </div>
 
-                        <div className="panel bg-blue-400">
-                            <div className="panel-body">
-                                <div className="heading-elements">
-                                    {/* <span className="heading-text badge bg-teal-800">+53,6%</span> */}
+                                    <h3 className="no-margin">6</h3>
+                                    Produk
+                                <div className="text-muted text-size-small">Total Jumlah Produk</div>
                                 </div>
 
-                                <h3 className="no-margin">6</h3>
-                                Produk
-                                <div className="text-muted text-size-small">Total Jumlah Produk</div>
+                                <div className="container-fluid">
+                                    <div id="members-online"></div>
+                                </div>
                             </div>
-
-                            <div className="container-fluid">
-                                <div id="members-online"></div>
-                            </div>
-                        </div>
-
+                        </Link>
                     </div>
                 </div>
             </Layout>
