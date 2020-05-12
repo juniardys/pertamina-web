03447d5d18efe08a18e65d2ba0a5f94f18d3edf9
Fariz <nizaralfariziakbar10@gmail.com>
2020-05-12 15:14:37 +0700

    new migration for mobile report

---
+++ database/migrations/1589269561327_feeder_tank_schema.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class FeederTankSchema extends Schema {
+  up () {
+    this.create('feeder_tanks', (table) => {
+      table.increments()
+      table.uuid('uuid')
+      table.uuid('spbu_uuid').references('uuid').inTable('spbu').onDelete('cascade').notNullable()
+      table.uuid('product_uuid').references('uuid').inTable('products').onDelete('cascade')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('feeder_tanks')
+  }
+}
+
+module.exports = FeederTankSchema
