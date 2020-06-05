ca21b12a595f2c71beb769201642b48ae2af9de3
Juniardy <juniardysetio@gmail.com>
2020-06-05 14:46:36 +0700

    Admin get feeder tank, nozzle, payment & co worker

--- app/Controllers/Http/V1/mobile/AdminReportController.js
+++ app/Controllers/Http/V1/mobile/AdminReportController.js
@@ -2,6 +2,7 @@
 
 const Shift = use('App/Models/Shift')
 const Island = use('App/Models/Island')
+const SpbuPayment = use('App/Models/SpbuPayment')
 const FeederTank = use('App/Models/FeederTank')
 const Nozzle = use('App/Models/Nozzle')
 const Delivery = use('App/Models/Delivery')
@@ -90,6 +91,151 @@ class AdminReportController {
             }
         }
         return response.status(200).json(baseResp(true, data, 'Data Shift Report sukses diterima'))
+    }
+
+    async getFeederTank({ request, response, transform }) {
+        const req = request.all()
+        const validation = await validate(req, {
+            spbu_uuid: 'required',
+            date: 'required',
+            shift_uuid: 'required'
+        })
+        if (validation.fails()) return response.status(400).json(baseResp(false, [], validation.messages()[0].message))
+
+        const feeder_tank = await FeederTank.query().where('spbu_uuid', req.spbu_uuid).with('product').fetch()
+        let data = []
+        for (let i = 0; i < feeder_tank.toJSON().length; i++) {
+            const nzl = feeder_tank.toJSON()[i];
+            const reportFeederTank = await ReportFeederTank.query()
+            .where('spbu_uuid', req.spbu_uuid)
+            .where('shift_uuid', req.shift_uuid)
+            .where('feeder_tank_uuid', nzl.uuid)
+            .where('date', moment(req.date).format('YYYY-MM-DD'))
+            .first()
+
+            if (!reportFeederTank) {
+                nzl['data'] = null
+            } else {
+                nzl['data'] = reportFeederTank.toJSON()
+                nzl['data']['date'] = moment(reportFeederTank.toJSON()['date']).format('YYYY-MM-DD HH:mm:ss')
+            }
+            data.push(nzl)
+        }
+
+        return response.status(200).json(baseResp(true, data, 'Data laporan tangki utama sukses diterima'))
+    }
+
+    async getNozzle({ request, response, transform }) {
+        const req = request.all()
+        const validation = await validate(req, {
+            spbu_uuid: 'required',
+            date: 'required',
+            shift_uuid: 'required',
+        })
+        if (validation.fails()) return response.status(400).json(baseResp(false, [], validation.messages()[0].message))
+        const getIsland = await Island.query().where('spbu_uuid', req.spbu_uuid).fetch()
+        const data = await transform.collection(getIsland, IslandTransformer)
+        for (let index = 0; index < data.length; index++) {
+            const island = data[index]
+            island['nozzle'] = new Array()
+            const nozzle = await Nozzle.query().where('spbu_uuid', req.spbu_uuid).where('island_uuid', island.uuid).fetch()
+            for (let i = 0; i < nozzle.toJSON().length; i++) {
+                const nzl = nozzle.toJSON()[i];
+                const reportNozzle = await ReportNozzle.query()
+                .where('spbu_uuid', req.spbu_uuid)
+                .where('island_uuid', island.uuid)
+                .where('shift_uuid', req.shift_uuid)
+                .where('nozzle_uuid', nzl.uuid)
+                .where('date', moment(req.date).format('YYYY-MM-DD'))
+                .first()
+    
+                if (!reportNozzle) {
+                    nzl['data'] = null
+                } else {
+                    nzl['data'] = reportNozzle.toJSON()
+                    nzl['data']['date'] = moment(reportNozzle.toJSON()['date']).format('YYYY-MM-DD HH:mm:ss')
+                }
+                island['nozzle'].push(nzl)
+            }
+        }
+
+        return response.status(200).json(baseResp(true, data, 'Data laporan pompa sukses diterima'))
+    }
+
+    async getPayment({ request, response, transform }) {
+        const req = request.all()
+        const validation = await validate(req, {
+            spbu_uuid: 'required',
+            date: 'required',
+            shift_uuid: 'required',
+        })
+        if (validation.fails()) return response.status(400).json(baseResp(false, [], validation.messages()[0].message))
+        const getIsland = await Island.query().where('spbu_uuid', req.spbu_uuid).fetch()
+        const data = await transform.collection(getIsland, IslandTransformer)
+        for (let index = 0; index < data.length; index++) {
+            const island = data[index]
+            island['payment'] = new Array()
+            const payment = await SpbuPayment.query().where('spbu_uuid', req.spbu_uuid).with('payment').fetch()
+            for (let i = 0; i < payment.toJSON().length; i++) {
+                const pymnt = payment.toJSON()[i];
+                let dataPayment = pymnt.payment
+                const reportPayment = await ReportPayment.query()
+                .where('spbu_uuid', req.spbu_uuid)
+                .where('island_uuid', island.uuid)
+                .where('shift_uuid', req.shift_uuid)
+                .where('payment_method_uuid', dataPayment.uuid)
+                .where('date', moment(req.date).format('YYYY-MM-DD'))
+                .first()
+
+                if (!reportPayment) {
+                    dataPayment['data'] = null
+                } else {
+                    dataPayment['data'] = reportPayment.toJSON()
+                    dataPayment['data']['date'] = moment(reportPayment.toJSON()['date']).format('YYYY-MM-DD HH:mm:ss')
+                }
+                island['payment'].push(nzl)
+            }
+        }
+        
+        return response.status(200).json(baseResp(true, data, 'Data laporan pembayaran sukses diterima'))
+    }
+
+    async getCoWorker({ request, response, transform }) {
+        const req = request.all()
+        const validation = await validate(req, {
+            spbu_uuid: 'required',
+            date: 'required',
+            shift_uuid: 'required'
+        })
+        if (validation.fails()) return response.status(400).json(baseResp(false, [], validation.messages()[0].message))
+        const getIsland = await Island.query().where('spbu_uuid', req.spbu_uuid).fetch()
+        const data = await transform.collection(getIsland, IslandTransformer)
+        for (let index = 0; index < data.length; index++) {
+            const island = data[index]
+            island['co_worker'] = {
+                data: [],
+                checklist: []
+            }
+            const co_worker = await User.query().where('spbu_uuid', req.spbu_uuid).where('role_uuid', '0bec0af4-a32f-4b1e-bfc2-5f4933c49740').fetch()
+            for (let i = 0; i < co_worker.toJSON().length; i++) {
+                const co_work = co_worker.toJSON()[i];
+                co_work['checked'] = false
+                let checked = await ReportCoWorker.query()
+                    .where('spbu_uuid', req.spbu_uuid)
+                    .where('island_uuid', req.island_uuid)
+                    .where('shift_uuid', req.shift_uuid)
+                    .where('user_uuid', co_work.uuid)
+                    .where('date', moment(req.date).format('YYYY-MM-DD'))
+                    .getCount()
+                if (checked > 0) {
+                    co_work['checked'] = true
+                    island['co_worker'].checklist.push(co_work.uuid)
+                }
+                island['co_worker'].data.push(co_work)
+            }
+        }
+
+        return response.status(200).json(baseResp(true, data, 'Data laporan rekan kerja sukses diterima'))
     }
 
     async store({ request, response, transform, auth }) {
