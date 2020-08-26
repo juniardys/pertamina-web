2faef267f5fd85cbf1ed310f4ccce434fd3f7a91
Juniardy Setiowidayoga <juniardysetio@gmail.com>
2020-08-26 14:25:47 +0700

    remove email unique

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -10,7 +10,7 @@ class UserSchema extends Schema {
       table.uuid('uuid').unique().notNullable()
       table.uuid('spbu_uuid').references('uuid').inTable('spbu').nullable()
       table.uuid('role_uuid').references('uuid').inTable('roles').onDelete('cascade').notNullable()
-      table.string('email', 254).notNullable().unique()
+      table.string('email', 254).notNullable()
       table.string('password', 60).notNullable()
       table.string('name', 254).notNullable()
       table.string('phone').nullable()
